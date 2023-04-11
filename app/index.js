1ff22d0a395dfd6521352ce1a9f41dcccd85fc45
Carlos ALvarez <carlosmail1978@gmail.com>
2023-04-11 20:12:14 +0200

    Add console log

--- app/index.js
+++ app/index.js
@@ -2,8 +2,18 @@ const strictEquals = (valueA, valueB) => {
   if (Number.isNaN(valueA) && Number.isNaN(valueB)) {
     return false;
   }
+  if (!valueA > valueB || !valueB > valueB) {
+    return true;
+  }
 
   const result = Object.is(valueA, valueB);
   return result;
 };
-strictEquals();
+console.log(strictEquals(1, 1));
+console.log(strictEquals(NaN, NaN));
+console.log(strictEquals(0, -0));
+console.log(strictEquals(-0, 0));
+console.log(strictEquals(1, "1"));
+console.log(strictEquals(true, false));
+console.log(strictEquals(false, false));
+console.log(strictEquals("water", "oil"));
